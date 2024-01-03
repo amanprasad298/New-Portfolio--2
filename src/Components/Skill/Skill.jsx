60080d35dda3cc4a28549bf0689b51d080c40db1
bihari <amanprasad1502@gmail.com>
2024-01-03 21:42:27 +0530

    skill commit

--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -4,30 +4,61 @@ import './Skill.css'
 const Skill = () => {
   return (
     <div className='wrap-skill'>
-        <hr />
-        <div className="container py-5">
-            <p>What I do for you?</p>
-            <div className="wrapper-skill">
+        
+        <div className="text-marquee">
+        <h1>What I do for you? &lt;/&gt;   </h1>
+        <h1>What I do for you? &lt;/&gt;  </h1>
+        <h1>What I do for you? &lt;/&gt;  </h1>
+        <h1>What I do for you? &lt;/&gt;   </h1>
+        <h1>What I do for you? &lt;/&gt;   </h1>
+        </div>
 
-                <div className="d-flex align-items-center justify-content-between skill-text">
-                    <h2>Graphic Design</h2>
-                    <p>I craft MVPs that captivate, securing funding and customers through innovative design and strategic positioning.</p>
+        <div className="container d-flex justify-content-center"><hr /></div>
+        
+        <div className="container item-wrap d-flex justify-content-center align-items-center">
+
+            
+
+            <div className="row">
+            <div className="col-lg-3 col-md-6 col-sm-12">
+            <div className="box ">
+                <div className="content">
+                    <h2>01</h2>
+                    <h3>Graphic Design</h3>
+                    <p>Craft MVPs that captivate, securing funding and customers through innovative design and strategic positioning.</p>
                 </div>
+            </div>
+            </div>
 
-                <div className="d-flex align-items-center justify-content-between skill-text">
-                    <h2>UI Design</h2>
-                    <p>I specialize in creating visually appealing designs that enhance user experience, blending aesthetics with functionality.</p>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+            <div className="box ">
+                <div className="content">
+                    <h2>02</h2>
+                    <h3>UI Design</h3>
+                    <p>Specialize in creating visually appealing designs that enhance UX, blending aesthetics with functionality.</p>
                 </div>
+            </div>
+            </div>
 
-                <div className="d-flex align-items-center justify-content-between skill-text">
-                    <h2>Content Writing</h2>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+            <div className="box ">
+                <div className="content">
+                    <h2>03</h2>
+                    <h3>Content Writing</h3>
                     <p>Crafting compelling content that leaves a lasting impression, from blog articles to brand messaging.</p>
                 </div>
+            </div>
+            </div>
 
-                <div className="d-flex align-items-center justify-content-between skill-text">
-                    <h2>Frontend Development</h2>
+            <div className="col-lg-3 col-md-6 col-sm-12">
+            <div className="box ">
+                <div className="content">
+                    <h2>04</h2>
+                    <h3>Frontend Dev</h3>
                     <p>Translating design visions into interactive web experiences with responsive layouts and seamless user interactions.</p>
                 </div>
+            </div>
+            </div>
 
             </div>
         </div>
